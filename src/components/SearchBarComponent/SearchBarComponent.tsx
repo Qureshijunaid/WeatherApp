7940dd6af83557a2172b2e2f26b24c6e7e83d35e
Qureshijunaid <qureshijunaid727@gmail.com>
2025-04-25 16:14:26 +0530

    Search Bar component

--- src/components/SearchBarComponent/SearchBarComponent.tsx
+++ src/components/SearchBarComponent/SearchBarComponent.tsx
@@ -1,9 +1,5 @@
-import {
-  Image,
-  TextInput,
-  TouchableOpacity,
-  View,
-} from "react-native";
+import React from "react";
+import { Image, TextInput, TouchableOpacity, View } from "react-native";
 
 import styles from "./styles";
 import { useTheme } from "../../context/ThemeContext";
@@ -16,27 +12,31 @@ type Props = {
 };
 
 const SearchBar = ({ value, onChange, onSearch }: Props) => {
-
-    const { theme } = useTheme();
-    const colors = theme === "light" ? lightTheme : darkTheme;
+  const { theme } = useTheme();
+  const colors = theme === "light" ? lightTheme : darkTheme;
 
   return (
-    <View style={[styles.container,{backgroundColor:colors.background}]}>
+    <View style={[styles.container, { backgroundColor: colors.background }]}>
       <TextInput
-        placeholderTextColor={colors.text }
-        style={[styles.input,{color:colors.text}]}
+        placeholderTextColor={colors.text}
+        style={[styles.input, { color: colors.text }]}
         value={value}
         onChangeText={onChange}
         placeholder="Enter the city name here..."
+        returnKeyType="search"
+        onSubmitEditing={onSearch}
       />
       <TouchableOpacity
         accessibilityRole="button"
+        accessibilityLabel="Search Button"
+        accessibilityHint="Search for the entered city"
+        hitSlop={{ top: 10, bottom: 10, left: 10, right: 10 }}
         disabled={value.length < 2}
         onPress={onSearch}
-        style={[styles.button,{backgroundColor:colors.buttonBackground}]}
+        style={[styles.button, { backgroundColor: colors.buttonBackground }]}
       >
         <Image
-          style={[styles.icon,{tintColor:colors.background}]}
+          style={[styles.icon, { tintColor: colors.background }]}
           source={require("../../assets/icon/searchicon.png")}
         />
       </TouchableOpacity>
